8fc15cfc57f7114f712ee226d508f133c234b9b6
samshiv26 <samshiv26@gmail.com>
2025-09-16 15:32:52 +0530

    vectorsUnion

--- 2.cpp
+++ 2.cpp
@@ -1,5 +1,24 @@
-#include<iostream>
+#include <iostream>
+#include <vector>
+#include <set>
 using namespace std;
-int main(){
-    cout<<"sahul";
+
+int main() {
+    int n1, n2;
+    cin >> n1;
+    vector<int> v1(n1);
+    for (int i = 0; i < n1; i++) cin >> v1[i];
+
+    cin >> n2;
+    vector<int> v2(n2);
+    for (int i = 0; i < n2; i++) cin >> v2[i];
+
+    set<int> unionSet;
+    for (int x : v1) unionSet.insert(x);
+    for (int x : v2) unionSet.insert(x);
+
+    for (int x : unionSet) cout << x << " ";
+    cout << endl;
+
+    return 0;
 }
